5e6f3d61ec20a1ced1e28fdf2f0ae5fcdadbfdd4
isabelPatulski <isab985d@stud.kea.dk>
2022-05-17 10:39:11 +0200

    Login og register igang

--- fetchUtils.js
+++ fetchUtils.js
@@ -1,16 +1,17 @@
-export function makeOptions(method, body) {
-    const opts = {
-      method: method, 
-      headers: {
-        "Content-type": "application/json",
-        "Accept": "application/json"
-      }
-    }
 
+export function makeOptions(method, body, addToken) {
+    const opts = {
+        method: method,
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        }
+    }
+    if (addToken) {
+        opts.headers.Authorization = "Bearer " + sessionStorage.getItem("token")
+    }
     if (body) {
-      opts.body = JSON.stringify(body);
+        opts.body = JSON.stringify(body);
     }
     return opts;
-  }
-
-  
+}
---
+++ js-pages/register/register.js
@@ -0,0 +1,26 @@
+import { showPage } from "../utils.js"
+import {LOCAL_SERVER_URL} from "../settings.js"
+import {makeOptions} from "../../fetchUtils";
+
+const URL = LOCAL_SERVER_URL + "/auth/register"
+
+async function registerCustomer() {
+    const customer = {}
+    customer.email = document.getElementById("input-email").value
+    customer.username = document.getElementById("input-username").value
+    customer.birthday = document.getElementById("input-birthday").value
+    customer.password = document.getElementById("input-password").value
+
+
+    fetch(URL, makeOptions("POST", customer, true))
+        .then(res => {
+            if (!res.ok) {
+                return Promise.reject("Error: " + res.status)
+            }
+            return res.json()
+        })
+        .then(newCar => {
+            document.getElementById("car-info-all").innerText = JSON.stringify(newCar)
+        })
+        .catch(e => console.error(e))
+}
