63e74fdd5cb98d5a09b478a2d0fd670a261390aa
isabelPatulski <isab985d@stud.kea.dk>
2022-05-19 15:42:08 +0200

    Register og login rettet til med rigtige id

--- js-pages/login/login.js
+++ js-pages/login/login.js
@@ -1,42 +1,37 @@
-import {LOCAL_SERVER_URL} from "../../settings.js"
-import {fetchData} from "./fetchProtectedData.js";
-import {makeOptions, handleErrors} from "../../fetchUtils.js";
-
-const URL = LOCAL_SERVER_URL + "customers/login"
+import { showPage } from "../utils.js"
+import { SERVER_URL } from "../settings.js"
+import { handleErrors, makeOptions } from "../fetchUtils.js"
 
 export function setupLoginHandlers() {
     document.getElementById("btn-login").onclick = login
 }
 
-async function login() {
-    const customer = {}
-    customer.userName = document.getElementById("username").value
-    customer.password = document.getElementById("password").value
+async function login(evt) {
+    evt.preventDefault()
+    const credentials = {}
+    credentials.email = document.getElementById("email").value
+    credentials.password = document.getElementById("password").value
+    const options = makeOptions("POST", credentials)
+    try {
+        const response = await fetch(SERVER_URL + "customer/login", options)
+            .then(res => handleErrors(res))
 
-    fetch(URL, makeOptions("POST", customer))
-        .then(res => {
-            if (!res.ok) {
-                if (res.status === 401) {
-                    return Promise.reject("Wrong user name or password")
-                }
-            }
-            return res.json()
-        })
-        .then(response => {
-            const token = response.token;
-            setLoginState(token)
-        })
-        .catch(e => {
-            document.getElementById("login-err").innerText = e;
-        })
-
+        const token = response.token
+        const role = response.roles[0]
+        setLoginState(token, role)
+        showPage("page-about")
+    } catch (err) {
+        document.getElementById("error").innerText = err.message + " - Try again"
+    }
 }
+
 
 export function logout() {
     setLoginState(null)
+    showPage("page-about")
 }
 
-function setLoginState(token, loggedInAs) {
+export function setLoginState(token, loggedInAs) {
     if (token) {
         sessionStorage.setItem("token", token)
         if (loggedInAs) {
@@ -46,5 +41,17 @@ function setLoginState(token, loggedInAs) {
         sessionStorage.clear("token")
         sessionStorage.clear("logged-in-as")
     }
+    updateLoginDependentComponents()
 }
 
+export function updateLoginDependentComponents() {
+    const loggedIn = sessionStorage.getItem("token")
+    const loggedInAs = sessionStorage.getItem("logged-in-as")
+    document.getElementById("user-role").innerText = ""
+    if (loggedIn) {
+        document.getElementById("user-role").innerText = "Logged in as: " + loggedInAs
+    }
+    document.getElementById("logged-in").style.display = loggedIn ? "block" : "none"
+    document.getElementById("page-login").style.display = loggedIn ? "none" : "block"
+    document.getElementById("page-logout").style.display = loggedIn ? "block" : "none"
+}
--- js-pages/register/register.js
+++ js-pages/register/register.js
@@ -1,7 +1,7 @@
 import {LOCAL_SERVER_URL} from "../../settings.js"
 import {makeOptions} from "../../fetchUtils.js";
 
-const URL = LOCAL_SERVER_URL + "customers"
+const URL = LOCAL_SERVER_URL + "customer/register"
 
 export function setupRegisterHandlers() {
     document.getElementById("btn-register").onclick = registerCustomer
@@ -15,15 +15,10 @@ function registerCustomer() {
     customer.password = document.getElementById("input-password").value
 
 
-    fetch(URL, makeOptions("POST", customer, true))
-        .then(res => {
-            if (!res.ok) {
-                return Promise.reject("Error: " + res.status)
-            }
-            return res.json()
-        })
+    fetch(URL, makeOptions("POST", customer))
+        .then(res => res.json())
         .then(newCustomer => {
-            document.getElementById("customer-info-all").innerText = JSON.stringify(newCustomer)
+            document.getElementById("btn-register").innerText = JSON.stringify(newCustomer)
         })
-        .catch(e => console.error(e))
+        .catch(error => console.error(error))
 }
